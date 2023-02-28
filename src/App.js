cbba29ee8093b599d1e08d5bbed5062aacdc76b1
ThienMenhVN <92326414+ThienMenhVN@users.noreply.github.com>
2023-03-01 01:02:29 +0700

    TestIntent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import './css/Reset.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Content">
+        <div className="Nav">
+          <a href="#" className="Nav_tag Nav_tag-all">All</a>
+          <a href="#" className="Nav_tag Nav_tag-manpower">Manpower Supply</a>
+          <a href="#" className="Nav_tag Nav_tag-mobile">Mobile App / Websites</a>
+          <a href="#" className="Nav_tag Nav_tag-design">UI/UX Design</a>
+        </div>
+        <div className="ListItem">
+          <a href='#' className="ListItem-Item">
+            <div className="Item">
+              <div className="Item-title">
+                <h6 className='Item-title_detail'>TOI 3D Customize E-commerce</h6>
+                <i class="fa-solid fa-chevron-right Item-icon" ></i>
+              </div>
+            </div>
+          </a>
+          <a href='#' className="ListItem-Item">
+            <div className="Item">
+              <div className="Item-title">
+                <h6 className='Item-title_detail'>TOI 3D Customize E-commerce</h6>
+                <i class="fa-solid fa-chevron-right Item-icon" ></i>
+              </div>
+            </div>
+          </a>
+          <a href='#' className="ListItem-Item">
+            <div className="Item">
+              <div className="Item-title">
+                <h6 className='Item-title_detail'>TOI 3D Customize E-commerce</h6>
+                <i class="fa-solid fa-chevron-right Item-icon" ></i>
+              </div>
+            </div>
+          </a>
+          <a href='#' className="ListItem-Item">
+            <div className="Item">
+              <div className="Item-title">
+                <h6 className='Item-title_detail'>TOI 3D Customize E-commerce</h6>
+                <i class="fa-solid fa-chevron-right Item-icon" ></i>
+              </div>
+            </div>
+          </a>
+          <a href='#' className="ListItem-Item">
+            <div className="Item">
+              <div className="Item-title">
+                <h6 className='Item-title_detail'>TOI 3D Customize E-commerce</h6>
+                <i class="fa-solid fa-chevron-right Item-icon" ></i>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
